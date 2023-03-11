ae02fce204467a2e028ed1352f4a5938526583a0
master <hanvipno9@gmail.com>
2023-03-11 16:07:03 +0700

    success model

--- src/main/java/project2022/javaswing/models/BookRoom.java
+++ src/main/java/project2022/javaswing/models/BookRoom.java
@@ -5,10 +5,99 @@
  */
 package project2022.javaswing.models;
 
+import java.sql.Date;
+
 /**
  *
  * @author Ching
  */
 public class BookRoom {
+    private int madatphong;
+    private int manhanvien;
+    private int makhachhang;
+    private int maphong;
+    private Date ngaydat;
+    private Date ngaytra;
+    private int tonggiatri;
+    private int trangthai;
+
+    public BookRoom(int madatphong, int manhanvien, int makhachhang, int maphong, Date ngaydat, Date ngaytra, int tonggiatri, int trangthai) {
+        this.madatphong = madatphong;
+        this.manhanvien = manhanvien;
+        this.makhachhang = makhachhang;
+        this.maphong = maphong;
+        this.ngaydat = ngaydat;
+        this.ngaytra = ngaytra;
+        this.tonggiatri = tonggiatri;
+        this.trangthai = trangthai;
+    }
+
+    public BookRoom() {
+    }
+
+    public int getMadatphong() {
+        return madatphong;
+    }
+
+    public void setMadatphong(int madatphong) {
+        this.madatphong = madatphong;
+    }
+
+    public int getManhanvien() {
+        return manhanvien;
+    }
+
+    public void setManhanvien(int manhanvien) {
+        this.manhanvien = manhanvien;
+    }
+
+    public int getMakhachhang() {
+        return makhachhang;
+    }
+
+    public void setMakhachhang(int makhachhang) {
+        this.makhachhang = makhachhang;
+    }
+
+    public int getMaphong() {
+        return maphong;
+    }
+
+    public void setMaphong(int maphong) {
+        this.maphong = maphong;
+    }
+
+    public Date getNgaydat() {
+        return ngaydat;
+    }
+
+    public void setNgaydat(Date ngaydat) {
+        this.ngaydat = ngaydat;
+    }
+
+    public Date getNgaytra() {
+        return ngaytra;
+    }
+
+    public void setNgaytra(Date ngaytra) {
+        this.ngaytra = ngaytra;
+    }
+
+    public int getTonggiatri() {
+        return tonggiatri;
+    }
+
+    public void setTonggiatri(int tonggiatri) {
+        this.tonggiatri = tonggiatri;
+    }
+
+    public int getTrangthai() {
+        return trangthai;
+    }
+
+    public void setTrangthai(int trangthai) {
+        this.trangthai = trangthai;
+    }
+    
     
 }
--- src/main/java/project2022/javaswing/models/Room.java
+++ src/main/java/project2022/javaswing/models/Room.java
@@ -10,5 +10,65 @@ package project2022.javaswing.models;
  * @author Ching
  */
 public class Room {
+    private int maphong;
+    private String tenphong;
+    private int loaiphong;
+    private int gia;
+    private int trangthai;
+
+    public Room(int maphong, String tenphong, int loaiphong, int gia, int trangthai) {
+        this.maphong = maphong;
+        this.tenphong = tenphong;
+        this.loaiphong = loaiphong;
+        this.gia = gia;
+        this.trangthai = trangthai;
+    }
+
+    public Room() {
+    }
+
+    public int getMaphong() {
+        return maphong;
+    }
+
+    public void setMaphong(int maphong) {
+        this.maphong = maphong;
+    }
+
+    public String getTenphong() {
+        return tenphong;
+    }
+
+    public void setTenphong(String tenphong) {
+        this.tenphong = tenphong;
+    }
+
+    public int getLoaiphong() {
+        return loaiphong;
+    }
+
+    public void setLoaiphong(int loaiphong) {
+        this.loaiphong = loaiphong;
+    }
+
+    public int getGia() {
+        return gia;
+    }
+
+    public void setGia(int gia) {
+        this.gia = gia;
+    }
+
+    public int getTrangthai() {
+        return trangthai;
+    }
+
+    public void setTrangthai(int trangthai) {
+        this.trangthai = trangthai;
+    }
+    
+    
+    
+    
     
 }
