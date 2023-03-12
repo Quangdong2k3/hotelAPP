fa9019f6c58aae499824e0d7f66858c6f89d749c
master <hanvipno9@gmail.com>
2023-03-12 18:44:43 +0700

    model room

--- src/main/java/project2022/javaswing/dao/RoomTypeDao.java
+++ src/main/java/project2022/javaswing/dao/RoomTypeDao.java
@@ -5,7 +5,13 @@
  */
 package project2022.javaswing.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import project2022.javaswing.config.DBConnection;
 import project2022.javaswing.models.RoomType;
 
 /**
@@ -16,28 +22,84 @@ public class RoomTypeDao implements DAO<RoomType> {
 
     @Override
     public ArrayList<RoomType> getAll() {
+        String sql = "SELECT * FROM loaiphong;";
+        ArrayList<RoomType> list = new ArrayList<RoomType>();
+        try {
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                RoomType rt = new RoomType(rs.getInt("maloaiphong"), rs.getString("tenloaiphong"));
+                list.add(rt);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomTypeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
     }
 
     @Override
     public RoomType getDetail(RoomType t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM loaiphong; WHERE maloaiphong = ?;";
+        RoomType rt = new RoomType();
+        try {
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                rt.setMaloaiphong(rs.getInt("maloaiphong"));
+                rt.setTenloaiphong(rs.getString("tenloaiphong"));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomTypeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rt;
     }
 
     @Override
     public void insertDao(RoomType t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "INSERT INTO loaiphong (tenloaiphong) VALUES (?);";
+
+        try {
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1, t.getTenloaiphong());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomTypeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void updateDao(RoomType t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "UPDATE loaiphong SET tenloaiphong = ? WHERE maloaiphong = ?;";
+        try {
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1, t.getTenloaiphong());
+            ps.setInt(2, t.getMaloaiphong());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomTypeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void deleteDao(RoomType t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            //delete in phong why foreign key
+            String sql = "DELETE FROM phong WHERE loaiphong = ?";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+
+            ps.setInt(1, t.getMaloaiphong());
+            ps.executeUpdate();
+
+            //delete in loaiphong why foreign key
+            sql = "DELETE FROM loaiphong WHERE maloaiphong = ?;";
+            ps.setInt(1, t.getMaloaiphong());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomTypeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
-    
-    
 }
